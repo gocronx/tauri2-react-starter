70f0ff078c4c5ef6622470d1d03580683fa470eb
gocronx <gocronx@gmail.com>
2026-08-06 19:11:51 +0800

    fix(windows): disable tauri_build manifest and set bin test=false

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,9 +1,13 @@
 fn main() {
     #[cfg(windows)]
     {
+        let attrs = tauri_build::Attributes::new()
+            .windows_attributes(tauri_build::WindowsAttributes::new_without_app_manifest());
         embed_manifest::embed_manifest(embed_manifest::new_manifest("tauri2-react-starter"))
             .expect("unable to embed manifest");
+        tauri_build::try_build(attrs).expect("failed to run tauri_build");
     }
 
+    #[cfg(not(windows))]
     tauri_build::build();
 }

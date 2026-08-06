203dec969f62d655c38821a4005d8a05a0c0535d
gocronx <gocronx@gmail.com>
2026-08-06 19:03:59 +0800

    fix(windows): embed Common Controls manifest via embed-manifest to fix 0xc0000139 in cargo test

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,3 +1,9 @@
 fn main() {
+    #[cfg(windows)]
+    {
+        embed_manifest::embed_manifest(embed_manifest::new_manifest("tauri2-react-starter"))
+            .expect("unable to embed manifest");
+    }
+
     tauri_build::build();
 }

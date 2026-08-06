9f5c4ca8c04536bec837485beec6d18bcad0003a
gocronx <gocronx@gmail.com>
2026-08-06 20:29:55 +0800

    fix(windows): embed Windows manifest via MSVC linker flags for tests

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,13 +1,15 @@
 fn main() {
     #[cfg(windows)]
     {
-        let attrs = tauri_build::Attributes::new()
-            .windows_attributes(tauri_build::WindowsAttributes::new_without_app_manifest());
-        embed_manifest::embed_manifest(embed_manifest::new_manifest("tauri2-react-starter"))
-            .expect("unable to embed manifest");
-        tauri_build::try_build(attrs).expect("failed to run tauri_build");
+        let manifest =
+            std::path::Path::new(env!("CARGO_MANIFEST_DIR")).join("windows-app-manifest.xml");
+        println!("cargo:rerun-if-changed={}", manifest.display());
+        println!("cargo:rustc-link-arg=/MANIFEST:EMBED");
+        println!(
+            "cargo:rustc-link-arg=/MANIFESTINPUT:{}",
+            manifest.to_str().unwrap()
+        );
     }
 
-    #[cfg(not(windows))]
     tauri_build::build();
 }

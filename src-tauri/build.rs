b6f9f55f9570862454c552558c998ec977bf820e
gocronx <gocronx@gmail.com>
2026-08-06 18:43:12 +0800

    fix(windows): embed common controls manifest in build.rs for cargo test compatibility

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,3 +1,25 @@
 fn main() {
-    tauri_build::build()
+    #[cfg(windows)]
+    {
+        let mut res = winres::WindowsResource::new();
+        res.set_manifest(r#"
+<assembly xmlns="urn:schemas-microsoft-com:asm.v1" manifestVersion="1.0">
+<dependency>
+    <dependentAssembly>
+        <assemblyIdentity
+            type="win32"
+            name="Microsoft.Windows.Common-Controls"
+            version="6.0.0.0"
+            processorArchitecture="*"
+            publicKeyToken="6595b64144ccf1df"
+            language="*"
+        />
+    </dependentAssembly>
+</dependency>
+</assembly>
+"#);
+        let _ = res.compile();
+    }
+
+    tauri_build::build();
 }
